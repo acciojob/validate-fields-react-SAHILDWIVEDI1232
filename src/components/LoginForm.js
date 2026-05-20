ab275c6b04e267bdca0782d349c83a9871101c5e
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-20 11:06:10 +0530

    Create LoginForm.js

---
+++ src/components/LoginForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+function LoginForm(){
+  const [input,setInput]=useState("Enter Text")
+return(
+  <form>
+  <div id="errorMessage" 
+  style={{backgroundColor:"red",text:"white"}}></div>
+  <div id="username"
+  <input
+  type="text"
+    value={input}
+        onChange={(e) => setInput(e.target.value)}>
+          </div>
+            <div id="password"
+  <input
+  type="text"
+    value={input}
+        onChange={(e) => setInput(e.target.value)}>
+          </div>
+
+          if(id==="errorMessage"){
+            console.log("errrorMessage")
+  </form>
+)
+}
