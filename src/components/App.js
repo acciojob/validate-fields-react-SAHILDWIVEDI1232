71312f4eb8edef8e22a3a89e91e511cdd66cdc44
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-20 11:07:18 +0530

    Add LoginForm component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import LoginForm from "./LoginForm"
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <LoginForm />
     </div>
   )
 }
